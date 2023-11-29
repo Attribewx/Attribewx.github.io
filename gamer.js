90c022e0cfd87282ccc0905331f812ada26493cd
Attribewx <98205105+Attribewx@users.noreply.github.com>
2023-11-29 03:29:12 -0800

    Update gamer.js

--- gamer.js
+++ gamer.js
@@ -1,4 +1,4 @@
-const scrollers = document.querySelctorAll(".scroller");
+const scrollers = document.querySelectorAll(".scroller");
 
 if(!window.matchMedia("(prefers-reduced-motion: reduce)").matches)
 {
